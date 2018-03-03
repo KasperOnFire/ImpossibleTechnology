eaf2588e313197d00d074d0e4d1c4c51872a76f6
Alfen321 <tjalfe.m@gmail.com>
2018-03-03 18:53:42 +0100

    fixed question 4 + some comments

--- Assignment3/run.py
+++ Assignment3/run.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 file_name = "KoreanConflict.csv"
 KoreanConflict = pd.read_csv(file_name)
+# repalce all empty values with an empty string to help np.unique not crash
 KoreanConflict = KoreanConflict.replace(np.nan,'',regex=True)
 kc = KoreanConflict.as_matrix()
 
@@ -22,7 +23,6 @@ def question1():
         plt.text(a, b, str(b), horizontalAlignment="center")
 
 
-#comments
 def question2():
     enrollment, count = np.unique(kc[:, 2], return_counts=True)
     # and plot it!
@@ -36,8 +36,8 @@ def question2():
 
 
 def question3():
-    # fuckin errors, i DUNNO
     ethnicity, count = np.unique(kc[:, 15], return_counts=True)
+    # Clean up data to make it more readable
     for i in range(len(ethnicity)):
         temp = ethnicity[i] 
         temp = temp.split('(')[0] 
@@ -58,12 +58,22 @@ def question3():
 
 def question4():
     division, count = np.unique(kc[:, 18], return_counts=True)
+    
+    # Limit the amount of divisions shown because 1799 is too many to show
+    limit = 20
+    # Sort divisions based on count (-count used to reverse so it's descend)
+    division = division[np.argsort(-count)][:limit]
+    count = np.sort(count)[::-1][:limit]
 
+    division[0] = 'NaN'
+
+    # and plot it!
     plt.figure("Question 4")
     plt.title("Which division had the most casualties?", fontSize=12)
     plt.xlabel("Division", fontSize=12)
     plt.ylabel("Losses", fontSize=12)
     plt.bar(division, count)
+    plt.xticks(rotation=90)
     for a, b in zip(division, count):
         plt.text(a, b, str(b), horizontalAlignment="center")
 
@@ -72,6 +82,7 @@ def question5():
     mask = (kc[:, 11] == "US")
     us_soldiers = kc[mask]
     state, count = np.unique(us_soldiers[:, 13], return_counts=True)
+    # and plot it!
     plt.figure("Question 5")
     plt.title("Which Home state suffered the most losses?", fontSize=12)
     plt.xlabel("State", fontSize=12)

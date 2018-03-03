2920632677267918f3ac674204458e8d4d6a0c28
Kasper Ravn Breindal <kasperbreindal@gmail.com>
2018-03-03 14:17:22 +0100

    made all tasks

--- Assignment3/run.py
+++ Assignment3/run.py
@@ -10,13 +10,12 @@ kc = KoreanConflict.as_matrix()
 def question1():
     # Get unique branches, and the count of every branch
     branches, count = np.unique(kc[:, 3], return_counts=True)
-
     # and plot it!
     plt.figure("Question 1")
     plt.title(
         "How many soldiers entered from a marine corps branch?", fontSize=12)
-    plt.xlabel("branch", fontSize=12)
-    plt.ylabel("count", fontSize=12)
+    plt.xlabel("Branch", fontSize=12)
+    plt.ylabel("Count", fontSize=12)
     plt.bar(branches, count)
     for a, b in zip(branches, count):
         plt.text(a, b, str(b), horizontalAlignment="center")
@@ -24,12 +23,11 @@ def question1():
 
 def question2():
     enrollment, count = np.unique(kc[:, 2], return_counts=True)
-
     # and plot it!
     plt.figure("Question 2")
     plt.title("Which enrollment was the most common?", fontSize=12)
     plt.xlabel("Enrollment", fontSize=12)
-    plt.ylabel("count", fontSize=12)
+    plt.ylabel("Count", fontSize=12)
     plt.bar(enrollment, count)
     for a, b in zip(enrollment, count):
         plt.text(a, b, str(b), horizontalAlignment="center")
@@ -37,22 +35,49 @@ def question2():
 
 def question3():
     # fuckin errors, i DUNNO
-    #ethnicity, count = np.unique(kc[:, 15], return_counts=True)
+    ethnicity, count = np.unique(kc[:, 15], return_counts=True)
     # and plot it!
     plt.figure("Question 3")
     plt.title(
-        "Was there an ethnicity majority throughout the war - If so, which?",
-        fontSize=12)
-    plt.xlabel("ethnicity", fontSize=12)
-    plt.ylabel("count", fontSize=12)
+        "Was there an ethnicity majority throughout the war?", fontSize=12)
+    plt.xlabel("Ethnicity", fontSize=12)
+    plt.ylabel("Losses", fontSize=12)
     plt.bar(ethnicity, count)
     for a, b in zip(ethnicity, count):
+        plt.text(a, b, str(b), horizontalAlignment="center")
+
+
+def question4():
+    division, count = np.unique(kc[:, 18], return_counts=True)
+
+    plt.figure("Question 4")
+    plt.title("Which division had the most casualties?", fontSize=12)
+    plt.xlabel("Division", fontSize=12)
+    plt.ylabel("Losses", fontSize=12)
+    plt.bar(division, count)
+    for a, b in zip(division, count):
+        plt.text(a, b, str(b), horizontalAlignment="center")
+
+
+def question5():
+    mask = (kc[:, 14] == "US")
+    us_soldiers = kc[mask]
+    state, count = np.unique(us_soldiers[:, 16], return_counts=True)
+
+    plt.figure("Question 5")
+    plt.title("Which Home state suffered the most losses?", fontSize=12)
+    plt.xlabel("State", fontSize=12)
+    plt.ylabel("Losses", fontSize=12)
+    plt.bar(state, count)
+    for a, b in zip(state, count):
         plt.text(a, b, str(b), horizontalAlignment="center")
 
 
 # run every highorder function
 question1()
 question2()
-#question3()
+question3()
+question4()
+question5()
 
 plt.show()

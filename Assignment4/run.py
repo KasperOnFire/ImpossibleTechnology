9d0504ac162a6945f4ee7f631150311819f3f22c
Kasper Ravn Breindal <kasperbreindal@gmail.com>
2018-03-10 12:56:16 +0100

    merge mit shiet

--- Assignment4/run.py
+++ Assignment4/run.py
@@ -1,11 +1,62 @@
-import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
-import webget
+import pandas as pd
+import webget as wg
 
-data_set_url = "https://raw.githubusercontent.com/INFINITE-KH/Python-Dataset/master/complete.csv"
-file_name = webget.download(data_set_url)
+file_link = "https://www.kaggle.com/kevinmh/fifa-18-more-complete-player-dataset/downloads/complete.csv/5"
+file_name = "fifaplayers.csv"
+#wg.download(file_link, "fifatest.csv")
+FifaPlayers = pd.read_csv(file_name)
+fp = FifaPlayers.as_matrix()
 
-f18pdf = pd.read_csv(file_name)
-f18pmatrix = f18pdf.as_matrix()
 
+def question2():
+    #get nationalities and count of each
+    nationalities, count = np.unique(fp[:, 14], return_counts=True)
+    #get only top 15
+    limit = 15
+
+    #sort both lists, in descending order for highest first
+    nationalities = nationalities[np.argsort(-count)][:limit]
+    count = np.sort(count)[::-1][:limit]
+
+    #plot it
+    plt.figure("Question 2")
+    plt.title("Which nationality is the most common amongst all players?")
+    plt.xlabel("Nationality", fontSize=8)
+    plt.ylabel("Count", fontSize=12)
+    plt.bar(nationalities, count)
+    plt.xticks(rotation=70)
+    plt.tight_layout()
+    for a, b in zip(nationalities, count):
+        plt.text(a, b, str(b), horizontalAlignment="center")
+
+
+def question3():
+    players = []
+    diff = []
+    limit = 20
+    for player in fp:
+        players.append(player[1])
+        diff.append(player[18] - player[17])
+
+    players = players[np.argsort(-diff)][:limit]
+    diff = np.sort(diff)[::-1][:limit]
+
+    plt.figure("Question 3")
+    plt.title(
+        "What is the difference of the release clause and value of all players?"
+    )
+    plt.xlabel("Player", fontSize=8)
+    plt.ylabel("Difference", fontSize=12)
+    plt.bar(players, diff)
+    plt.xticks(rotation=70)
+    plt.tight_layout()
+    for a, b in zip(players, diff):
+        plt.text(a, b, str(b), horizontalAlignment="center")
+
+
+question2()
+question3()
+
+plt.show()

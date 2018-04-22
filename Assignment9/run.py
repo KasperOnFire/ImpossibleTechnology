973dfe97a409d2eedfaedf8a4f3063d564984252
Alfen321 <tjalfe.m@gmail.com>
2018-04-22 13:43:03 +0200

    q3,q4 and q5 done needs plotting

--- Assignment9/run.py
+++ Assignment9/run.py
@@ -4,17 +4,18 @@ import pandas as pd
 import webget as wg
 import datetime as dt
 
+
 obama_tweet_url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/twitter-ratio/BarackObama.csv"
 trump_tweet_url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/twitter-ratio/realDonaldTrump.csv"
 obama_tweet_csv = "Obama.csv"
 trump_tweet_csv = "Trump.csv"
-wg.download(obama_tweet_url, obama_tweet_csv)
-wg.download(trump_tweet_url, trump_tweet_csv)
+# wg.download(obama_tweet_url, obama_tweet_csv)
+# wg.download(trump_tweet_url, trump_tweet_csv)
 
-obama_tweets_df = pd.read_csv(obama_tweet_csv, sep=',', parse_dates=[0], index_col=[1], encoding = "ISO-8859-1")
+obama_tweets_df = pd.read_csv(obama_tweet_csv, sep=',', parse_dates=[0], encoding = "ISO-8859-1")
 obama_tweets_matrix = obama_tweets_df.as_matrix()
 
-trump_tweets_df = pd.read_csv(trump_tweet_csv, sep=',', parse_dates=[0], index_col=[1], encoding = "ISO-8859-1")
+trump_tweets_df = pd.read_csv(trump_tweet_csv, sep=',', parse_dates=[0], encoding = "ISO-8859-1")
 trump_tweets_matrix = trump_tweets_df.as_matrix()
 
 def question_1():
@@ -44,6 +45,32 @@ def question_2():
     print("Trump tweets per week in 2017")
     print(trump_tweet_17_count)
 
+def question_3():
+    print(count_containging(['yes we can', 'make america great again'], [obama_tweets_df.text, trump_tweets_df.text]))
+
+def question_4():
+    print(count_containging(['Iran','Iran'], [obama_tweets_df.text, trump_tweets_df.text]))
+
+def question_5():
+    print(count_containging(['Obamacare','Obamacare'], [obama_tweets_df.text, trump_tweets_df.text]))
+
+def count_containging(substrings, arrays):
+    counts = []
+    for i in range(len(arrays)):
+        counts.append(substring_count_in_array(substrings[i], arrays[i]))
+    return counts
+
+
+def substring_count_in_array(substring, array):
+    count = 0
+    for el in array:
+        if substring.lower() in el.lower():
+            count += 1
+    return count
+
+
 question_1()
 question_2()
-    
+question_3()
+question_4()
+question_5()
